b0eff9536e4aad64bc1bfeeecd18a99439beca7a
Pau Moreno <pmoreno2022@ilg.cat>
2022-11-07 17:07:59 +0100

    2Commit

--- Projecte1/TestGit.java
+++ Projecte1/TestGit.java
@@ -1,6 +1,7 @@
 package git01;
 public class TestGit {
 		public static void main(String[] args) { 
-			System.out.println("This is a message.");
+			String name = "Bugs Bunny";
+			System.out.println("Hello " + name + "!");
 		}
 }
