5fab35519fb6abd601b9b71392ec4bbe7e42f2a6
Pau Moreno <pmoreno2022@ilg.cat>
2022-11-07 17:29:54 +0100

    5Commit

--- Projecte1/TestGit.java
+++ Projecte1/TestGit.java
@@ -1,7 +1,7 @@
 package git01;
 public class TestGit {
-		public static void main(String[] args) { 
-			String name = "Bugs Bunny";
-			System.out.println("Hello " + name.toUpperCase() + "!");
-		}
+	public static void main(String[] args) { 
+		String name = "Bugs Bunny";
+		System.out.println("Hello Mr. " + name.toUpperCase() + "!");
+	}
 }
